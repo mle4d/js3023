a1f382e700af916ce5b86259a06a7514a55ddd7e
emily <mle4d@me.com>
2020-02-28 22:28:32 -0800

    move SR above SS

--- index.js
+++ index.js
@@ -26,46 +26,7 @@ function follow(evt) {
     }
 document.onmousemove = follow;
 
-const msg = new SpeechSynthesisUtterance();
-  let voices = [];
-  const voicesList = document.querySelector('[name="voice"]');
-  const options = document.querySelectorAll('[type="range"], [name="text"]');
-  // const speakButton = document.querySelector('#say');
-  const playButton = document.querySelector('#play');
-  msg.text = document.querySelector('.speech').data;
-
-  function populateVoices() {
-    voices = this.getVoices();
-    voicesList.innerHTML = voices
-      .filter(voice => voice.name.length < 10)
-      .filter(voice => voice.lang.includes('en'))
-      .map(voice => `<option value="${voice.name}">${voice.name}</option>`)
-      .join('');
-  }
-
-  function setVoice() {
-    msg.voice = voices.find(voice => voice.name === this.value);
-    toggle();
-  }
-
-  function setOption() {
-    msg[this.name] = this.value;
-    toggle();
-  }
-  function toggle(startOver = true) {
-    speechSynthesis.cancel();
-    if (startOver) {
-      speechSynthesis.speak(msg);
-    }
-  }
-
-  speechSynthesis.addEventListener('voiceschanged', populateVoices);
-  voicesList.addEventListener('click', setVoice);
-  options.forEach(option => option.addEventListener('change', setOption));
-  // sayButton.addEventListener('click', toggle);
-  playButton.addEventListener('click', toggle);
-
-  window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
   
   const recognition = new SpeechRecognition();
   recognition.interimResults = true;
@@ -90,3 +51,45 @@ const msg = new SpeechSynthesisUtterance();
       }
   });
   recognition.start();
+
+const msg = new SpeechSynthesisUtterance();
+  let voices = [];
+  const voicesList = document.querySelector('[name="voice"]');
+  const options = document.querySelectorAll('[type="range"], [name="text"]');
+  // const speakButton = document.querySelector('#say');
+  const playButton = document.querySelector('#play');
+  msg.text = document.querySelector('.speech').value;
+  console.log(msg);
+
+  function populateVoices() {
+    voices = this.getVoices();
+    voicesList.innerHTML = voices
+      .filter(voice => voice.name.length < 10)
+      .filter(voice => voice.lang.includes('en'))
+      .map(voice => `<option value="${voice.name}">${voice.name}</option>`)
+      .join('');
+  }
+
+  function setVoice() {
+    msg.voice = voices.find(voice => voice.name === this.value);
+    toggle();
+  }
+
+  function setOption() {
+    msg[this.name] = this.value;
+    toggle();
+  }
+  function toggle(startOver = true) {
+    speechSynthesis.cancel();
+    if (startOver) {
+      speechSynthesis.speak(speech);
+    }
+  }
+
+  speechSynthesis.addEventListener('voiceschanged', populateVoices);
+  voicesList.addEventListener('click', setVoice);
+  options.forEach(option => option.addEventListener('change', setOption));
+  // sayButton.addEventListener('click', toggle);
+  playButton.addEventListener('click', toggle);
+
+  
