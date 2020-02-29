c6d8bc0a72724329451c60e76ea0f7ad73e0424b
emily <mle4d@me.com>
2020-02-29 02:40:13 -0800

    succesfully got sr into a textarea

--- recognition.js
+++ recognition.js
@@ -4,22 +4,30 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
   recognition.interimResults = true;
   recognition.lang = 'en-US';
   
-  let p = document.createElement('p');
-  const words = document.querySelector('.speech');
-  words.appendChild(p);
+  // let p = document.createElement('p');
+  // const words = document.querySelector('.speech');
+  // words.appendChild(p);
 
-  recognition.addEventListener('result', e => {
-    const transcript = Array.from(e.results)
-      .map(result => result[0])
-      .map(result => result.transcript)
-      .join('');
+  // recognition.addEventListener('result', e => {
+  //   const transcript = Array.from(e.results)
+  //     .map(result => result[0])
+  //     .map(result => result.transcript)
+  //     .join('');
 
-      const Script = transcript.replace();
-      p.textContent = Script;
+  //     const Script = transcript.replace();
+  //     p.textContent = Script;
 
-      if (e.results[0].isFinal) {
-        p = document.createElement('p');
-        words.appendChild(p);
+  //     if (e.results[0].isFinal) {
+  //       p = document.createElement('p');
+  //       words.appendChild(p);
+  //     }
+      recognition.onresult = function (event) {
+        for (var i = event.resultIndex; i < event.results.length; ++i) {
+          if (event.results[i].isFinal) {
+            text.value += event.results[i][0].transcript;
+          }
+        }
       }
-  });
+      // msg(result.transcript)
+  
   recognition.start();
