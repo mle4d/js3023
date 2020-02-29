b7dc898b0e415962d4681e8ba9289fbe0e07f632
emily <mle4d@me.com>
2020-02-28 23:15:09 -0800

    renamed file

---
+++ synthesis.js
@@ -0,0 +1,41 @@
+const msg = new SpeechSynthesisUtterance();
+  let voices = [];
+  const voicesList = document.querySelector('[name="voice"]');
+  const options = document.querySelectorAll('[type="range"], [name="text"]');
+  // const speakButton = document.querySelector('#say');
+  const playButton = document.querySelector('#play');
+  msg.text = document.querySelector('.msg').value;
+  console.log(msg);
+
+  function populateVoices() {
+    voices = this.getVoices();
+    voicesList.innerHTML = voices
+      .filter(voice => voice.name.length < 10)
+      .filter(voice => voice.lang.includes('en'))
+      .map(voice => `<option value="${voice.name}">${voice.name}</option>`)
+      .join('');
+  }
+
+  function setVoice() {
+    msg.voice = voices.find(voice => voice.name === this.value);
+    toggle();
+  }
+
+  function setOption() {
+    msg[this.name] = this.value;
+    toggle();
+  }
+  function toggle(startOver = true) {
+    speechSynthesis.cancel();
+    if (startOver) {
+      speechSynthesis.speak(msg);
+    }
+  }
+
+  speechSynthesis.addEventListener('voiceschanged', populateVoices);
+  voicesList.addEventListener('click', setVoice);
+  options.forEach(option => option.addEventListener('change', setOption));
+  // sayButton.addEventListener('click', toggle);
+  playButton.addEventListener('click', toggle);
+
+  
