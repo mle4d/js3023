7652eeed3cb2784d3e9a83b8f9b4018e811db268
emily <mle4d@me.com>
2020-02-29 16:01:08 -0800

    delete commented out code

--- recognition.js
+++ recognition.js
@@ -3,34 +3,15 @@ window.SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecogn
   const recognition = new SpeechRecognition();
   recognition.interimResults = true;
   recognition.lang = 'en-US';
-  
-  // let p = document.createElement('p');
-  // const words = document.querySelector('.speech');
-  // words.appendChild(p);
 
-  // recognition.addEventListener('result', e => {
-  //   const transcript = Array.from(e.results)
-  //     .map(result => result[0])
-  //     .map(result => result.transcript)
-  //     .join('');
-
-  //     const Script = transcript.replace();
-  //     p.textContent = Script;
-
-  //     if (e.results[0].isFinal) {
-  //       p = document.createElement('p');
-  //       words.appendChild(p);
-  //     }
-      recognition.onresult = function (event) {
-        for (var i = event.resultIndex; i < event.results.length; ++i) {
-          if (event.results[i].isFinal) {
-            text.value += event.results[i][0].transcript;
-            console.log(value);
-            const Script = transcript.replace();
-            textarea.textContent = Script;
-          }
+  recognition.onresult = function (event) {
+    for (var i = event.resultIndex; i < event.results.length; ++i) {
+    if (event.results[i].isFinal) {
+      text.value += event.results[i][0].transcript;
+        const script = transcript.replace();
+          text.value = script;
         }
       }
+    }
      
-  
   recognition.start();
